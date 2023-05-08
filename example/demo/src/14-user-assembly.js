5c2ab2a7d8b9fcbf6243f41cef36ff2515c5e8da
wangyongxin <676425826@qq.com>
2023-05-08 20:51:37 +0800

    组件的使用-嵌套和复用

---
+++ example/demo/src/14-user-assembly.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProFile() {
+  return (
+    <img
+    src="https://i.imgur.com/MK3eW3As.jpg"
+    alt="Katherine Johnson"
+  />
+  )
+}
+export default function ProfileUse() {
+  return (
+    <seciton>
+      <h1>了不起的科学家</h1>
+      <ProFile/>
+      <ProFile/>
+      <ProFile/>
+    </seciton>
+  )
+}
