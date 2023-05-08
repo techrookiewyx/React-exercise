bc1223d0c202bdd6c66e898ba4e6f3630d542fea
wangyongxin <676425826@qq.com>
2023-05-08 20:48:25 +0800

    组件创建

---
+++ example/demo/src/13-assembly.js
@@ -0,0 +1,10 @@
+import React from "react"
+export default function ProfileF() {
+  return (
+    <img
+      src="https://p5.ssl.qhimgs1.com/sdr/400__/t01a033d10bf4623115.jpg"
+      alt="钢铁侠"
+    />
+  )
+}
+
