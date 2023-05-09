1ee0806fb1522e33334abaaab751e3e0c6c0ff95
wangyongxin <676425826@qq.com>
2023-05-09 17:46:35 +0800

    组件默认导出和导入

---
+++ example/demo/src/16-defalut-exprot.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { SpecificExprot } from './15-exprot'
+
+
+export function Test(){
+  return (
+     <p style={{color:"red"}}>我是默认导出中的具名导出</p>
+   )
+}
+export default function DefExprot() {
+  return (
+    <div>
+      <h2>我是默认导出</h2>
+      <p>下面是具名导入的内容:</p>
+        <SpecificExprot />
+        <SpecificExprot />
+        <SpecificExprot />
+    </div>
+  )
+}
