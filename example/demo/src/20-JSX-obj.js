6f97efa0fac8310ce286f19ac657189738685d87
wangyongxin <676425826@qq.com>
2023-05-09 20:43:08 +0800

    JSX大括号变对象嵌入

---
+++ example/demo/src/20-JSX-obj.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function JsxObj() {
+  return (
+    <ul style={{background:'black',color:'pink'}}>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
+  )
+}
