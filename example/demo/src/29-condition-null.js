d73c6031e7296c2fe5c1758e41bb0c0080a297af
wangyongxin <676425826@qq.com>
2023-05-10 20:36:05 +0800

    提交渲染-返回null

---
+++ example/demo/src/29-condition-null.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Item({name,isPacked}){
+  if (isPacked) { 
+    return null
+  }
+  return  <li>{name}</li>
+}
+export default function JudgeNull() {
+  return (
+    <div>
+      <h1>Sally Ride的行李清单</h1>
+      <ul>
+        <Item name='宇航服' isPacked={true}/>
+        <Item name='带金箔的头盔' isPacked={true}/>
+        <Item name='Tam的照片' isPacked={false}/>
+      </ul>
+    </div>
+  )
+}
