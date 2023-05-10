8220692a7a4618f178283daf8a7c8cc091423499
wangyongxin <676425826@qq.com>
2023-05-10 18:01:28 +0800

    为列表添加key

--- example/demo/src/JSX5-list-render.js
+++ example/demo/src/JSX5-list-render.js
@@ -8,8 +8,8 @@ const div = <div>{arr1}</div>  //显示 张三李四王五
 
 //以列表形式在页面中显示数组中的元素
 //本质就是需要根据["张三", "李四", "王五"]创建[<li>张三</li>，<li>李四</li>，<li>王五</li>]
-const newArr = arr1.map(person =>
-  <li>
+const newArr = arr1.map((person,index) =>
+  <li key={index}>
     {person}
   </li>
 );
