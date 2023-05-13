ee56531cf89df3537a3f6ff02d80739889222c76
wangyongxin <676425826@qq.com>
2023-05-13 20:40:50 +0800

    更新state后替换

---
+++ example/demo/src/68-update-replact.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export default function ReplaceStateUpd() {
+  const [number, setNumber] = useState(0);
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={() => { 
+        setNumber(number+5);
+        setNumber(n => n + 1);
+        setNumber(40);
+      }}>
+      增加数字 </button>
+    </div>
+  );
+}
