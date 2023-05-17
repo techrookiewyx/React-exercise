f7cb7117fe18ff10e7a2f1b65f2eb1c44ac2b143
wangyongxin <676425826@qq.com>
2023-05-17 20:43:13 +0800

    深度嵌套的的state

---
+++ example/demo/src/102-deep-nest-state.jsx
@@ -0,0 +1,32 @@
+import { initialTravelPlan } from "./places-data";
+import { useState } from "react";
+
+function PlaceTree({place}){
+  const childPlaces = place.childPlaces;
+  return(
+    <li>
+      {place.title}
+      {childPlaces.length > 0 &&
+        <ol>
+          {childPlaces.map(ele =>
+            <PlaceTree key={ele.id} place={ele}/>
+          )}
+        </ol>
+      }
+    </li>
+  )
+}
+export default function DeepNestState() {
+  const [plan, setPlan] = useState(initialTravelPlan);
+  const planets = plan.childPlaces;
+  return (
+    <div>
+      <h2>Places to visit</h2>
+      <ol>
+        {planets.map(ele =>
+          <PlaceTree key={ele.id} place={ele}/>
+        )}
+      </ol>
+    </div>
+  )
+}
