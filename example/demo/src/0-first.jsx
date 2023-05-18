10bdeaa27b0bbef5311f1f1fd3897be6ec2c6dff
wangyongxin <676425826@qq.com>
2023-05-18 14:51:40 +0800

    第一个案例

---
+++ example/demo/src/0-first.jsx
@@ -0,0 +1,16 @@
+function MyButton(){
+  return (
+      <button>
+        按钮
+      </button>
+    )
+}
+
+export default function FirstProgram() { 
+  return (
+    <div>
+      <h1>我的第一个react程序</h1>
+      <MyButton /><br /><br />
+    </div>  
+  )
+}
