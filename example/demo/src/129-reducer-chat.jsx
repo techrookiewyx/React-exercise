5717c522e76d0abb48ae3861716aac428c732aa0
wangyongxin <676425826@qq.com>
2023-05-20 20:55:45 +0800

    reducer使用案例-切换收件人

---
+++ example/demo/src/129-reducer-chat.jsx
@@ -0,0 +1,68 @@
+import { useReducer } from "react";
+import { chuShi, messageReducer } from "./129-messageReducer";
+
+const contacts = [
+  {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
+  {id: 1, name: 'Alice', email: 'alice@mail.com'},
+  {id: 2, name: 'Bob', email: 'bob@mail.com'},
+];
+
+function List({ list, dispatch, selectedId }) { 
+  return (
+    <ul>
+      {list.map(ele=>
+        <li key={ele.id}>
+          <button onClick={() => {
+            dispatch({
+              type: 'select',
+              id: ele.id,
+            })
+          }}>
+            {selectedId===ele.id?
+              <b>{ele.name}</b> :
+              ele.name
+            }
+          </button>
+        </li>
+      )}
+    </ul>
+  )
+}
+function Chat({ mes, pep, dispatch }) {
+  return(
+    <div>
+      <textarea
+        placeholder={`和 ${pep.name} 聊天`}
+        value={mes}
+        onChange={e => { 
+          dispatch({
+            type: 'textarea_input',
+            message: e.target.value
+          })
+        }}
+      />
+      <br />
+      <button>发送到 {pep.email}</button>
+    </div>
+  )
+}
+export default function ReducerChat() { 
+  const [message, dispatch] = useReducer(messageReducer, chuShi);
+  const mes = message.message;
+  const people = contacts[message.selectedId];
+  return (
+    <div>
+      <List
+        list={contacts}
+        dispatch={dispatch}
+        selectedId={message.selectedId}
+      />
+      <Chat
+        pep={people}
+        mes={mes}
+        key={people.id}
+        dispatch={dispatch}
+      />
+    </div>
+  )
+}
