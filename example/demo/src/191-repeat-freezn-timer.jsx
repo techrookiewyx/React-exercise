1641ca690af2ac95840152651c0dbfe1bb8ba438
wangyongxin <676425826@qq.com>
2023-05-29 20:56:40 +0800

    effect事件案例-修复冻结计数器

---
+++ example/demo/src/191-repeat-freezn-timer.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { experimental_useEffectEvent as useEffectEvent } from 'react';
+export default function RepeatFreezeTimer() { 
+  const [count, setCount] = useState(0);
+  const [add, setAdd] = useState(1);
+  const updateCount = useEffectEvent(() => { 
+    setCount(c => c + add);
+  })
+  useEffect(()=>{
+    const id = setInterval(() => {
+      updateCount();
+    }, 1000);
+    return () => {clearInterval(id)}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+  return (
+    <>
+      <h1>
+        Counter: {count}
+        {" "}
+        <button onClick={()=>{setCount(0)}}>Reset</button>  
+      </h1>
+      <hr />
+      <div>
+        Every second, increment by:{" "}
+        <button disabled={add===0} onClick={()=>{setAdd(c=>c-1)}}>-</button>
+        <b>{add}</b>
+        <button onClick={()=>{setAdd(c=>c+1)}}>+</button>
+      </div>
+    </>
+  )
+}
---
+++ example/demo/src/192-repeat-delay.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export default function RepeatDelay() { 
+  const [add,setAdd] = useState(1);
+  const [count, setCount] = useState(0)
+  const [delay, setDelay] = useState(100);
+  useEffect(()=>{
+    const id =setInterval(() => {
+        setCount(c=>c+add)
+    }, delay);
+    return ()=> clearInterval(id)
+  },[delay,add])
+  return (
+    <>
+      <h1>Counter: {count}{" "}<button onClick={() => { setCount(0) }}>Reset</button></h1>
+      <hr/>
+      <div>Increment by:{" "}
+      <button disabled={add===0} onClick={()=>{setAdd(c=>c-1)}}>-</button>
+        <b>{add}</b>
+        <button onClick={()=>{setAdd(c=>c+1)}}>+</button>
+      </div>
+      <div>Increment delay::{" "}
+      <button disabled={delay===100} onClick={()=>{setDelay(c=>c-100)}}>–100 ms</button>
+        <b>{delay} ms</b>
+        <button onClick={()=>{setDelay(c=>c+100)}}>+100 ms</button>
+      </div>
+    </>
+  )
+}
