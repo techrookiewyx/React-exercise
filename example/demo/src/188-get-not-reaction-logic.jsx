8dbb93b5ae73c2b0da378af27f5c62ba6169c1f9
wangyongxin <676425826@qq.com>
2023-05-29 20:53:51 +0800

    effect事件提取非反应逻辑

---
+++ example/demo/src/188-get-not-reaction-logic.jsx
@@ -0,0 +1,51 @@
+import { showNotice } from "./187-notification";
+import { createConnection } from "./187-chat";
+import { useEffect, useState } from "react";
+import { experimental_useEffectEvent as useEffectEvent } from 'react';
+const serverUrl = "https://localhost:1234";
+
+function ChatRoom({ room, theme }) {
+  const onConn = useEffectEvent(() => { 
+    showNotice('Connected',theme)
+  })
+  useEffect(() => { 
+    const conn = createConnection(serverUrl, room);
+    conn.on('connected', () => { 
+      onConn();
+    })
+    conn.connect();
+    return ()=> conn.disconnect();
+  },[room,onConn])
+  return <h1>Welcome to the {room} room!</h1>
+}
+
+export default function EffectGetNotLogic() {
+  const [room, setRoom] = useState("general");
+  const [dark, setDark] = useState(false);
+  return (
+    <>
+      <label>
+        Choose the chat room:{" "}
+        <select value={room} onChange={(e) => setRoom(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        <input
+          type='checkbox'
+          checked={dark}
+          onChange={e => setDark(e.target.checked)}
+        />
+        {' Use dark theme'}
+      </label>
+      <hr />
+      <ChatRoom
+        room={room}
+        theme={dark ? 'dark' : 'light'}
+      />
+    </>
+  );
+}
