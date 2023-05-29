9d40e1f53d61f4830d5925933b6e6691f8611ff9
wangyongxin <676425826@qq.com>
2023-05-29 20:53:24 +0800

    effect中多余的反应逻辑

---
+++ example/demo/src/187-effect-reaction-logic.jsx
@@ -0,0 +1,47 @@
+import { showNotice } from "./187-notification";
+import { createConnection } from "./187-chat";
+import { useEffect, useState } from "react";
+const serverUrl = "https://localhost:1234";
+
+function ChatRoom({ room, theme }) {
+  useEffect(() => { 
+    const conn = createConnection(serverUrl, room);
+    conn.on('connected', () => { 
+      showNotice('Connected',theme)
+    })
+    conn.connect();
+    return ()=> conn.disconnect();
+  },[room,theme])
+  return <h1>Welcome to the {room} room!</h1>
+}
+
+export default function EffectOverLogic() {
+  const [room, setRoom] = useState("general");
+  const [dark, setDark] = useState(false);
+  return (
+    <>
+      <label>
+        Choose the chat room:{" "}
+        <select value={room} onChange={(e) => setRoom(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        <input
+          type='checkbox'
+          checked={dark}
+          onChange={e => setDark(e.target.checked)}
+        />
+        {' Use dark theme'}
+      </label>
+      <hr />
+      <ChatRoom
+        room={room}
+        theme={dark ? 'dark' : 'light'}
+      />
+    </>
+  );
+}
