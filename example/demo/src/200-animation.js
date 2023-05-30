fea84db440e0d2c0d73c6d897e1b2744472618fc
wangyongxin <676425826@qq.com>
2023-05-30 21:13:18 +0800

    动画实现类

---
+++ example/demo/src/200-animation.js
@@ -0,0 +1,33 @@
+export class FadeInAnimation{
+  constructor(node){
+    this.node = node
+  }
+  start(duration) {
+    this.duration = duration;
+    if (this.duration === 0) {
+      this.onProgress(1);
+    } else { 
+      this.onProgress(0);
+      this.startTime = performance.now();
+      this.frameId = requestAnimationFrame(() => this.onFrame());
+    }
+  }
+  onFrame() {
+    const timePassed = performance.now() - this.startTime;
+    const progress = Math.min(timePassed / this.duration, 1);
+    this.onProgress(progress);
+    if (progress < 1) {
+      this.frameId = requestAnimationFrame(() => this.onFrame());
+    }
+  }
+  onProgress(progress) {
+    this.node.style.opacity = progress;
+  }
+  stop() {
+    cancelAnimationFrame(this.frameId);
+    this.startTime = null;
+    this.frameId = null;
+    this.duration = 0;
+  }
+
+}
