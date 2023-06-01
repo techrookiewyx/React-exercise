125bcdc5de156950d21950a73db4513eb16ff098
wangyongxin <676425826@qq.com>
2023-06-01 20:57:10 +0800

    外部store

---
+++ example/demo/src/216-todoStore.js
@@ -0,0 +1,24 @@
+let nextId = 0;
+let todos = [{ id: nextId++, text:'Todo #' + nextId }]
+let listeners = [];
+
+export const todosStore = {
+  addTodo() {
+    todos = [...todos, { id: nextId++, text: 'Todo #' + nextId }];
+    emitChange();
+  },
+  subscribe(listener) {
+    listeners = [...listeners, listener];
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  },
+  getSnapshot() {
+    return todos;
+  }
+};
+function emitChange() { 
+  for (let listener of listeners) {
+    listener();
+  }
+}
