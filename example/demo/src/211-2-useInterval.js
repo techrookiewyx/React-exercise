1e002c243bb79c61ffbb7a79a105334aac4cc30a
wangyongxin <676425826@qq.com>
2023-06-01 20:53:47 +0800

    自定义hook-useInterval

---
+++ example/demo/src/211-2-useInterval.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+export function useInterval(onTick,delay) { 
+  useEffect(() => {
+    const id = setInterval(() => {
+      onTick();
+    }, delay);
+    return () => clearInterval(id);
+  },[onTick,delay])
+}
