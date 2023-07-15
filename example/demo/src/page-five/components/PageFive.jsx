b8084bdc2cf40bac94abeeb4d4022b9868eae1c2
wangyongxin <676425826@qq.com>
2023-07-15 23:39:31 +0800

    页面五内容区域

---
+++ example/demo/src/page-five/components/PageFive.jsx
@@ -0,0 +1,13 @@
+import { ContentBack } from "./ContentBack";
+import { LookBook } from "./LookBook";
+import { ShopList } from "./ShopList";
+
+export default function PageFiveDe(){
+  return (
+    <>
+      <ContentBack />
+      <ShopList/>
+      <LookBook/>
+    </>
+  )
+}
